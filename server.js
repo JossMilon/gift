b44c2d4127aed045d1e27cb0de7a84f525e69cd8
Josselin Milon <josselinmilon@MacBook-Pro-de-Josselin-2.local>
2021-12-09 00:47:33 +0100

    added static files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use("/assets", express.static(__dirname + "/assets"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
